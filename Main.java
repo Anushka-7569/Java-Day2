fe3daeb84229f43cd58b12bc1edc06c2648598aa
Anushka-7569 <anushka7569@gmail.com>
2026-02-07 10:57:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        String name="sandhya";
+        int age=15;
+        char grade='A';
+        int marks=45;
+        if(marks<50){
+            System.out.println("pass");
+        }
+        else{
+            System.out.println("fail");
+        }
+    
+        }
+        
+    }   
+
